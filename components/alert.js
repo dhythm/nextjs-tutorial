1d27c5b1a668cc8f6174092148e07aec1b479bf4
dhythm <Yuta.Okada.20@gmail.com>
2020-06-21 21:20:03 +0900

    proceeded the tutorial

---
+++ components/alert.js
@@ -0,0 +1,15 @@
+import cn from "classnames";
+import styles from "./alert.module.css";
+
+export default function Alert({ children, type }) {
+  return (
+    <div
+      className={cn({
+        [styles.success]: type === "success",
+        [styles.error]: type === "error",
+      })}
+    >
+      {children}
+    </div>
+  );
+}
