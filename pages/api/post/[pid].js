dcffc9ecc1c8cc34e929022e4829a0e3ee99e7bd
dhythm <Yuta.Okada.20@gmail.com>
2020-06-27 07:52:16 +0900

    update

--- pages/api/post/[pid].js
+++ pages/api/post/[pid].js
@@ -1,5 +1,25 @@
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 // http://localhost:3000/api/post/xxxx
-export default (req, res) => {
+export default async (req, res) => {
+  await runMiddleware(req, res, cors);
+
   const {
     query: { pid },
   } = req;
